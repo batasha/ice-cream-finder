7360e904e11f8990db220724d698e782f6a35a6a
Liz Starin and Natasha Badillo <liz+natasha@appacademy.io>
2013-04-25 15:11:34 -0400

    Works! But ugly.

--- ice_cream.rb
+++ ice_cream.rb
@@ -9,6 +9,8 @@ def get_current_location
 	puts "Want some ice cream?"
 	puts "Enter your current address:"
 	address = gets.chomp
+	puts "\n\nFind ice cream here:"
+	puts ""
 
 	location_query = Addressable::URI.new(
 	       :scheme => "http",
@@ -25,39 +27,27 @@ def get_current_location
 	location_raw["results"][0]["geometry"]["location"]
 end
 
+def get_nearby_shops(lat, long)
+	nearby_query = Addressable::URI.new(
+	       :scheme => "https",
+	       :host => "maps.googleapis.com",
+	       :path => "maps/api/place/nearbysearch/json",
+	       :query_values => {
+	                         :key => KEY,
+	                         :location => "#{lat},#{long}",
+	                         :rankby => "distance",
+													 #:radius => "1000",
+	                         :sensor => "false",
+													 :keyword => "ice cream"
+	                        }
+	                        ).to_s
 
+	nearby_raw = JSON.parse(RestClient.get(nearby_query))
 
-location = get_current_location
+	nearby_raw["results"]
+end
 
-lat = location["lat"]
-long = location["lng"]
-
-nearby_query = Addressable::URI.new(
-       :scheme => "https",
-       :host => "maps.googleapis.com",
-       :path => "maps/api/place/nearbysearch/json",
-       :query_values => {
-                         :key => KEY,
-                         :location => "#{lat},#{long}",
-                         :rankby => "distance",
-												 #:radius => "1000",
-                         :sensor => "false",
-												 :keyword => "ice cream"
-                        }
-                        ).to_s
-
-nearby_raw = JSON.parse(RestClient.get(nearby_query))
-
-nearby = nearby_raw["results"]
-
-
-nearby.each do |shop|
-	puts shop["name"]
-
-	dest_location = shop["geometry"]["location"]
-	dest_lat = dest_location["lat"]
-	dest_long = dest_location["lng"]
-
+def get_directions(lat, long, dest_lat, dest_long)
 	directions_query = Addressable::URI.new(
        :scheme => "http",
        :host => "maps.googleapis.com",
@@ -72,7 +62,27 @@ nearby.each do |shop|
 
 	directions_raw = JSON.parse(RestClient.get(directions_query))
 
-	directions = directions_raw["routes"][0]["legs"][0]["steps"]
+	directions_raw["routes"][0]["legs"][0]["steps"]
+end
+
+location = get_current_location
+
+lat = location["lat"]
+long = location["lng"]
+
+nearby = get_nearby_shops(lat, long)
+
+nearby.each do |shop|
+	puts shop["name"]
+
+	dest_location = shop["geometry"]["location"]
+	dest_lat = dest_location["lat"]
+	dest_long = dest_location["lng"]
+
+	directions = get_directions(lat, long, dest_lat, dest_long)
+
+
+
 	directions.each do |step|
 		puts step["html_instructions"]
 	end
